8f742ecbbb75f880fa657ebaefd0a2f8428fe2e2
Yazdan <yzdn.jt@gmail.com>
2022-01-23 17:20:58 +0330

    coordinates is done!

--- src/Data/Coordinates.java
+++ src/Data/Coordinates.java
@@ -1,4 +1,27 @@
 package Data;
 
 public class Coordinates {
+    private Integer x;
+    private Integer y;
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return '(' + x + ", " + y + ')';
+    }
 }
