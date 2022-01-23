e1ee16b5775c0e25be7ee8166e1888ddc70dbcac
Yazdan <yzdn.jt@gmail.com>
2022-01-23 23:21:44 +0330

    kdtree: add method is added + perorder printing

--- src/Data_Structures/KdTree.java
+++ src/Data_Structures/KdTree.java
@@ -1,4 +1,83 @@
 package Data_Structures;
 
+import Data.Bank;
+import Data.Coordinates;
+
+// Implementation of k-d tree
+// this tree contains Bank Nodes
 public class KdTree {
+
+    static class Node {
+        Bank bank;
+        Node r; // right Node
+        Node l; // left Node
+
+        Node(Bank bank) {
+            this.bank = bank;
+            r = l = null;
+        }
+    }
+
+    private final int k;
+    private Node root;
+    private int size;
+
+    KdTree() {
+        k = 2;
+        root = null;
+        size = 0;
+    }
+
+    private Node addNode(Node root, Bank bank, int level) {
+        if (root == null) {
+            return new Node(bank);
+        }
+
+        // based on x
+        if (level % k == 0) {
+            if (root.bank.getCoordinates().getX() > bank.getCoordinates().getX())
+                root.l = addNode(root.l, bank, level + 1);
+            else
+                root.r = addNode(root.r, bank, level + 1);
+        }
+        // based on y
+        else {
+            if (root.bank.getCoordinates().getY() > bank.getCoordinates().getY())
+                root.l = addNode(root.l, bank, level + 1);
+            else
+                root.r = addNode(root.r, bank, level + 1);
+        }
+
+        return root;
+    }
+
+    public void add(Bank bank) {
+        root = addNode(root, bank, 0);
+    }
+
+    public void printTreePreOrder(Node node) {
+        if (node == null) return;
+        System.out.println(node.bank);
+        printTreePreOrder(node.l);
+        printTreePreOrder(node.r);
+    }
+
+    public void printTreePreOrder(){
+        printTreePreOrder(root);
+    }
+
+
+    public static void main(String[] args) {
+        KdTree t = new KdTree();
+        //  (6, 12), (9, 1), (2, 7), (10, 19)
+        t.add(new Bank(new Coordinates(3, 6), "A"));
+        t.add(new Bank(new Coordinates(17, 15), "A"));
+        t.add(new Bank(new Coordinates(13, 15), "A"));
+        t.add(new Bank(new Coordinates(6, 12), "A"));
+        t.add(new Bank(new Coordinates(9, 1), "A"));
+        t.add(new Bank(new Coordinates(2, 7), "A"));
+        t.add(new Bank(new Coordinates(10, 19), "A"));
+        t.printTreePreOrder();
+
+    }
 }
