a6554ab30f968869608d618fa40595f4bb334eb5
Yazdan <yzdn.jt@gmail.com>
2022-01-24 12:51:51 +0330

    Coordinates : equal method is added to the class

--- src/Data/Coordinates.java
+++ src/Data/Coordinates.java
@@ -4,7 +4,7 @@ public class Coordinates {
     private final int x;
     private final int y;
 
-     public Coordinates(int x, int y) {
+    public Coordinates(int x, int y) {
         this.x = x;
         this.y = y;
     }
@@ -20,5 +20,10 @@ public class Coordinates {
     @Override
     public String toString() {
         return "(" + x + ", " + y + ")";
+    }
+
+    public boolean equals(Coordinates o) {
+        if (o == null) return false;
+        return this.getX() == o.getX() && this.getY() == o.getY();
     }
 }
