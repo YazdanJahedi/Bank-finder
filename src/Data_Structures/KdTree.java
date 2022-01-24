86ab0bb01f131b39958eff7820615252385caef7
Yazdan <yzdn.jt@gmail.com>
2022-01-24 12:52:10 +0330

    searching methods are added

--- src/Data_Structures/KdTree.java
+++ src/Data_Structures/KdTree.java
@@ -1,6 +1,8 @@
 package Data_Structures;
 
 import Data.Bank;
+import Data.BankBranch;
+import Data.Coordinates;
 
 // Implementation of k-d tree
 // this tree contains Bank Nodes
@@ -35,10 +37,13 @@ public class KdTree {
         return size;
     }
 
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     private Node addNode(Node root, Bank bank, int level) {
-        if (root == null) {
-            return new Node(bank);
-        }
+        if (root == null) return new Node(bank);
+//        if(root.bank.equals(bank)) {
+//            System.out.println("ERROR: this coordinates can not be used!");
+//            return root;
+//        }
 
         // based on x
         if (level % k == 0) {
@@ -62,7 +67,34 @@ public class KdTree {
         root = addNode(root, bank, 0);
         size++;
     }
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+    // just based on coordinates
+    private boolean findNode(Node root, Bank bank, int level) {
+        if (root == null) return false;
+        if (root.bank.equals(bank)) return true;
+
+        if (level % k == 0) {
+            if (root.bank.getCoordinates().getX() > bank.getCoordinates().getX())
+                return findNode(root.l, bank, level + 1);
+            else
+                return findNode(root.r, bank, level + 1);
+        }
+        // based on y
+        else {
+            if (root.bank.getCoordinates().getY() > bank.getCoordinates().getY())
+                return findNode(root.l, bank, level + 1);
+            else
+                return findNode(root.r, bank, level + 1);
+        }
+
+    }
+
+    public boolean find(Bank bank) {
+        return findNode(root, bank, 0);
+    }
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     private void printTreePreOrder(Node node) {
         if (node == null) return;
         System.out.println(node.bank);
@@ -72,6 +104,44 @@ public class KdTree {
 
     public void printTreePreOrder() {
         printTreePreOrder(root);
+    }
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+    public static void main(String[] args) {
+        KdTree t = new KdTree();
+        t.add(new Bank(new Coordinates(3, 6), "A"));
+        t.add(new Bank(new Coordinates(2, 7), "B"));
+        t.add(new Bank(new Coordinates(17, 15), "C"));
+        t.add(new Bank(new Coordinates(6, 12), "D"));
+        t.add(new Bank(new Coordinates(9, 1), "E"));
+        t.add(new Bank(new Coordinates(13, 15), "F"));
+        t.add(new Bank(new Coordinates(10, 19), "G"));
+
+        t.printTreePreOrder();
+        System.out.println(t.size());
+        System.out.println(t.find(new Bank(new Coordinates(17, 15), "C")));
+        System.out.println(t.find(new Bank(new Coordinates(17, 15), "T")));
+
+
+        System.out.println("! ! ! !! ! ! ! ! ! ! ! ");
+
+        KdTree tr = new KdTree();
+        tr.add(new BankBranch(new Coordinates(3, 6), "A", "A"));
+        tr.add(new BankBranch(new Coordinates(2, 7), "B", "B"));
+        tr.add(new BankBranch(new Coordinates(17, 15), "C", "C"));
+        tr.add(new BankBranch(new Coordinates(6, 12), "D", "D"));
+        tr.add(new BankBranch(new Coordinates(9, 1), "E", "E"));
+        tr.add(new BankBranch(new Coordinates(13, 15), "F", "F"));
+        tr.add(new BankBranch(new Coordinates(10, 19), "G", "G"));
+
+        tr.printTreePreOrder();
+        System.out.println(tr.size());
+        System.out.println(tr.find(new BankBranch(new Coordinates(17, 15), "C", "C")));
+        System.out.println(tr.find(new BankBranch(new Coordinates(3, 6), " " , " ")));
+        System.out.println(tr.find(new BankBranch(new Coordinates(10, 19), "C" , "R")));
+        System.out.println(tr.find(new BankBranch(new Coordinates(11, 19), "C" , "R")));
+        System.out.println(tr.find(new BankBranch(new Coordinates(17, 14), "C" , "R")));
     }
 
 }
