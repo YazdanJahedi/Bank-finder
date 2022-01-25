0c8ea40a92089c5aaedb43a7bcc137e673bdecb9
Yazdan <yzdn.jt@gmail.com>
2022-01-25 15:51:32 +0330

    hashing linked list : TESTED + bugs are fixed

--- src/Data_Structures/HashingLinkedList.java
+++ src/Data_Structures/HashingLinkedList.java
@@ -1,6 +1,8 @@
 package Data_Structures;
 
 import Data.Bank;
+import Data.CentralBank;
+import Data.Coordinates;
 
 public class HashingLinkedList {
     static class LinkedList {
@@ -81,7 +83,13 @@ public class HashingLinkedList {
     }
 
     // number of English alphabet letters
-    LinkedList[] hashTable = new LinkedList[26];
+    private final LinkedList[] hashTable = new LinkedList[26];
+
+    public HashingLinkedList() {
+        for (int i = 0; i < hashTable.length; i++) {
+            hashTable[i] = new LinkedList();
+        }
+    }
 
     public void add(Bank bank) {
         int firstChar = bank.getBankName().toLowerCase().charAt(0);
@@ -89,10 +97,41 @@ public class HashingLinkedList {
         hashTable[index].addFirst(bank);
     }
 
+    // search is based on bank's coordinates
     public Bank search(Bank bank) {
         int firstChar = bank.getBankName().toLowerCase().charAt(0);
         int index = firstChar - (int) 'a';
-        return hashTable[index].search(bank).bank;
+        LinkedList.Node ans = hashTable[index].search(bank);
+        if (ans != null) return ans.bank;
+        return null;
     }
 
+    public void print() {
+        for (LinkedList linkedList : hashTable) {
+            System.out.println(linkedList.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        HashingLinkedList a = new HashingLinkedList();
+        a.add(new CentralBank(new Coordinates(1, 2), "Abin"));
+        a.add(new CentralBank(new Coordinates(3, 4), "Alef"));
+        a.add(new CentralBank(new Coordinates(5, 6), "baba"));
+        a.add(new CentralBank(new Coordinates(6, 7), "bita"));
+        a.add(new CentralBank(new Coordinates(2, 3), "Boostan"));
+        a.add(new CentralBank(new Coordinates(1, 7), "cat"));
+        a.add(new CentralBank(new Coordinates(1, 7), "XX"));
+        a.add(new CentralBank(new Coordinates(1, 7), "categori"));
+        a.add(new CentralBank(new Coordinates(1, 7), "tiger"));
+        a.add(new CentralBank(new Coordinates(1, 7), "helloman"));
+        a.add(new CentralBank(new Coordinates(1, 7), "Zebra"));
+        a.print();
+        System.out.println("_________________________________________________________________");
+        System.out.println("_________________________________________________________________");
+        System.out.println(a.search(new CentralBank(new Coordinates(1, 7), "Zebra")));
+        System.out.println(a.search(new CentralBank(new Coordinates(1, 3), "Zebra")));
+        System.out.println(a.search(new CentralBank(new Coordinates(1, 7), "ali")));
+        System.out.println(a.search(new CentralBank(new Coordinates(1, 7), "tiger")));
+        System.out.println(a.search(new CentralBank(new Coordinates(1, 7), "tehran")));
+    }
 }
